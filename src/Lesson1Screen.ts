77138c4184ac742bc906bf08200408c0c6173fc9
awrose <awrose@udel.edu>
2023-01-21 13:17:53 -1000

    made the whiteboard bigger

--- src/Lesson1Screen.ts
+++ src/Lesson1Screen.ts
@@ -24,10 +24,10 @@ export default class Lesson1Screen extends Phaser.Scene {
         this.background = this.add.image(400, 300, 'beach');
         this.add.image(400, 62, 'topBanner');
         this.add.text(175, 40, 'Lesson 1: Data Types', { font: "bold 40px Arial", color: "white"});
-        this.add.image(175, 450, 'teacherTurtle');
-        this.add.image(500, 400, 'whiteboard');
-        this.skipButton = this.add.image(700, 550, 'skip');
-        this.add.text(645, 535, 'Skip Lesson', { font: "bold 20px Arial", color: "black"});
+        this.add.image(110, 450, 'teacherTurtle');
+        this.add.image(500, 425, 'whiteboard');
+        this.skipButton = this.add.image(700, 565, 'skip');
+        this.add.text(645, 550, 'Skip Lesson', { font: "bold 20px Arial", color: "black"});
         this.skipButton.setInteractive();
 
         //on click change to game scene 
