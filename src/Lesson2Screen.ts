643815f16b0cb26647f1c8e9a0f7bfbf61cbf22e
awrose <awrose@udel.edu>
2023-01-23 23:11:27 -1000

    added lesson plans 2 and 3 ***VERY ROUGH DRAFT****

--- src/Lesson2Screen.ts
+++ src/Lesson2Screen.ts
@@ -26,7 +26,14 @@ export default class Lesson2Screen extends Phaser.Scene {
         this.add.text(175, 40, 'Lesson 2: Indices', { font: "bold 40px Arial", color: "white"});
         this.add.image(110, 450, 'teacherTurtle');
         this.add.image(500, 425, 'whiteboard');
-        this.add.text(250, 200, 'TESTING', { font: "bold 25px Arial", color: "black"});
+            //35 characters long left --> right
+        //11 up to down  
+        //this.add.text(250, 200, '12345678912345678912345678902134567', { font: "bold 25px Arial", color: "black"});
+        this.add.text(250, 200, '**Insert picture here**', { font: "25px Arial", color: "black"});
+        this.add.text(250, 265, 'Start at 0 to count the dots in the\npicture How many did you get?\nYou should have gotten 3!\nComputers start counting at 0 instead of 1', { font: "25px Arial", color: "black"});
+
+        this.add.text(250, 380, 'Examples', { font: "bold 25px Arial", color: "black"});
+        this.add.text(250, 405, 'dots[0] is red\ndots[1] is yellow\ndots[2] is green\ndots[3] is blue', { font: "25px Arial", color: "black"});
         this.skipButton = this.add.image(700, 565, 'skip');
         this.add.text(645, 550, 'Skip Lesson', { font: "bold 20px Arial", color: "black"});
         this.skipButton.setInteractive();
--- src/Lesson3Screen.ts
+++ src/Lesson3Screen.ts
@@ -26,7 +26,13 @@ export default class Lesson3Screen extends Phaser.Scene {
         this.add.text(175, 40, 'Lesson 3: Conditionals', { font: "bold 40px Arial", color: "white"});
         this.add.image(110, 450, 'teacherTurtle');
         this.add.image(500, 425, 'whiteboard');
-        this.add.text(250, 200, 'TESTING', { font: "bold 25px Arial", color: "black"});        this.skipButton = this.add.image(700, 565, 'skip');
+        //35 characters long left --> right
+        //11 up to down  
+        this.add.text(250, 200, 'Just like you, computers have rules they have to follow\nWe tell the computer what to do by using If Statements\nIf statements are formatted like: If (true) Then (do this)\nWe use if statements every day!', { font: "20px Arial", color: "black"});     
+        this.add.text(250, 305, 'Examples --> THERE WILL BE PICTURES', { font: "bold 25px Arial", color: "black"});
+        this.add.text(250, 335, 'If (it\'s raining) Then: We bring an umbrella\nIf (it\'s cold) Then: We bring a jacket', { font: "20px Arial", color: "black"});
+        this.add.text(250, 470, 'Whatever comes after \'If\' must be true, before we\ncomplete the action after it', { font: "20px Arial", color: "black"});
+        this.skipButton = this.add.image(700, 565, 'skip');
         this.add.text(645, 550, 'Skip Lesson', { font: "bold 20px Arial", color: "black"});
         this.skipButton.setInteractive();
 
