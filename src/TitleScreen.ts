a5b61b334645fac125f38584d327d4d8b9ed984d
Himanshu Patel <hpatell@udel.edu>
2023-01-28 07:27:50 -1000

    add music and update title screen

--- src/TitleScreen.ts
+++ src/TitleScreen.ts
@@ -4,6 +4,10 @@ export default class TitleScreen extends Phaser.Scene {
 
     background?: Phaser.GameObjects.Image
     playnowButton?: Phaser.GameObjects.Image
+    musicButton?: Phaser.GameObjects.Image
+    musicOffButton?: Phaser.GameObjects.Image
+
+    isMute = false;
 
     constructor() 
     {
@@ -15,31 +19,131 @@ export default class TitleScreen extends Phaser.Scene {
         // load all images
         this.load.image('beach', 'assets/TitleScreen/beach.png');
         this.load.image('turtle', 'assets/TitleScreen/turtle.png');
-        this.load.image('turtle2', 'assets/TitleScreen/turtle2.png');
         this.load.image('playnow', 'assets/TitleScreen/playnow.png');
+        this.load.image('musicOn', 'assets/TitleScreen/musicOn.png');
+        this.load.image('musicOff', 'assets/TitleScreen/musicOff.png');
+        this.load.image('backdrop', 'assets/quiz/backdrop.png');
+
+        //load background music
+        this.load.audio('music', 'assets/audio/BackgroudMusic.mp3');
 	}
 
     create()
     {
-        this.background = this.add.image(400, 300, 'beach');
-        this.add.text(100, 50, 'Turbo Turtles', { font: "bold 95px Arial", color: '#964B00' });
-        this.add.image(350, 360, 'turtle');
-        this.add.image(120, 380, 'turtle2');
+        // add and create all the images and texts
+        this.background = this.add.image(400, 450, 'beach');
+        this.add.image(400, 90, 'backdrop');
+        this.add.text(100, 50, 'Turbo Turtles', { font: "bold 95px Arial", color: '#25851b' });
+        this.add.image(125, 500, 'turtle');
         this.playnowButton = this.add.image(400, 530, 'playnow');
         this.add.text(315, 500, 'Play Now', { font: "bold 40px Arial", color: 'black'});
         this.playnowButton.setInteractive();
 
         // change to QuizScene on click
         this.playnowButton.on('pointerup', () => {
-
             this.scene.start('Lesson1Screen');
 
 		})
+
+        // play music at start of game
+        this.sound.play('music');
+
+
+
+        if (this.isMute == false)
+        {
+            this.musicButton = this.add.image(750, 30, 'musicOff');
+            this.musicButton.scale = 0.09;
+            this.musicButton.setInteractive();
+        }
+        else 
+        {
+            this.musicButton = this.add.image(750, 30, 'musicOn');
+            this.musicButton.setInteractive();
+        }
+
+
+
     }
 
     update()
     {
+        // if (this.isMute == false)
+        // {
+        //     this.musicButton = this.add.image(750, 30, 'musicOff');
+        //     this.musicButton.scale = 0.09;
+        //     this.musicButton.setInteractive();
 
+        //     this.musicButton.on('pointerup', () => {
+        //         this.sound.stopAll();
+        //         this.musicButton?.destroy();
+        //         this.isMute = true;
+        //         this.musicButton = this.add.image(750, 30, 'musicOn');
+        //         this.musicButton.setInteractive();
+        //     })
+        // }
+        // else
+        // {
+        //     this.musicButton = this.add.image(750, 30, 'musicOn');
+        //     this.musicButton.setInteractive();
+
+        //     this.musicButton.on('pointerup', () => {
+        //         this.sound.play('music');
+        //         this.musicButton?.destroy();
+        //         this.isMute = false;
+        //         this.musicButton = this.add.image(750, 30, 'musicOff');
+        //         this.musicButton.scale = 0.09;
+        //         this.musicButton.setInteractive();
+        //     })
+        // }
+
+
+
+        // if (this.isMute == false)
+        // {
+        //     this.musicOffButton = this.add.image(750, 30, 'musicOff');
+        //     this.musicOffButton.scale = 0.09;
+        //     this.musicOffButton.setInteractive();
+
+        //     this.musicOffButton.on('pointerup', () => {
+        //         this.sound.stopAll();
+        //         this.musicOffButton?.destroy();
+        //         this.isMute = true;
+        //     })
+        // }
+        // else
+        // {
+        //     this.musicButton = this.add.image(750, 30, 'musicOn');
+        //     this.musicButton.setInteractive();
+
+        //     this.musicButton.on('pointerup', () => {
+        //         this.sound.play('music');
+        //         this.musicButton?.destroy();
+        //         this.isMute = false;
+        //     })
+        // }
+
+        
+
+        this.musicButton?.on('pointerup', () => {
+
+            if (this.isMute == false)
+            {
+                this.sound.stopAll();
+                this.musicButton?.destroy();
+                this.isMute = true;
+                this.musicButton = this.add.image(750, 30, 'musicOn');
+                this.musicButton.setInteractive();
+            }
+            else
+            {
+                this.sound.play('music');
+                this.musicButton?.destroy();
+                this.isMute = false;
+                this.musicButton = this.add.image(750, 30, 'musicOff');
+                this.musicButton.scale = 0.09;
+                this.musicButton.setInteractive();
+            }
+        })
     }
-
 }
